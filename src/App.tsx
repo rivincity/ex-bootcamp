9c293541303d824d170779e21b9862f29fd225c4
Rivan Parikh <rivan.parikh@gmail.com>
2024-10-03 19:00:07 -0400

    added random movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const buttonRef = useRef(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -13,6 +18,26 @@ const App: React.FC = () => {
     }
   };
 
+  const hoverRef = (ref: any) => {
+    if(ref.current) {
+      ref.current.addEventListener("mouseover", () => {
+        const randomX = Math.floor(Math.random() * (window.innerWidth - ref.current.offsetWidth));
+        const randomY = Math.floor(Math.random() * (window.innerHeight - ref.current.offsetHeight));
+
+        ref.current.style.position = 'absolute';
+        ref.current.style.left = `${randomX}px`;
+        ref.current.style.top = `${randomY}px`;
+      });
+    }
+  };
+
+  useEffect(() => {
+    hoverRef(nameRef);
+    hoverRef(emailRef);
+    hoverRef(passwordRef);
+    hoverRef(buttonRef);
+  }, []);
+
   return (
     <div className="App">
       {isLoggedIn ? (
@@ -20,7 +45,7 @@ const App: React.FC = () => {
       ) : (
         <form onSubmit={handleSubmit}>
           <h1>Login</h1>
-          <div>
+          <div id = "name" ref = {nameRef}>
             <label htmlFor="name">Name:</label>
             <input
               type="text"
@@ -30,7 +55,17 @@ const App: React.FC = () => {
               required
             />
           </div>
-          <div>
+          <div id = "email" ref = {emailRef}>
+            <label htmlFor="name">Email:</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div id = "password" ref = {passwordRef}>
             <label htmlFor="password">Password:</label>
             <input
               type="password"
@@ -40,7 +75,7 @@ const App: React.FC = () => {
               required
             />
           </div>
-          <button type="submit">Enter</button>
+          <button type="submit" ref = {buttonRef}>Enter</button>
         </form>
       )}
     </div>
